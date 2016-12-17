392dbb4f596b6ef9084390ecb62e7ebf2b8bbc94
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2016-12-17 05:38:18 -0500

    Create dothework.jl

---
+++ src/dothework.jl
@@ -0,0 +1,77 @@
+
+# do the work
+
+function readable_nonneg_integer{I<:Integer}(s::String, digits_spanned::I, group_separator::Char)
+    n = length(s)
+    n==0 && return "0"
+
+    sinteger, sexponent =
+        if contains(s,"e")
+           split(s,'e')
+        else
+           s, ""
+        end
+
+    n = length(sinteger)
+
+    fullgroups, finalgroup = divrem(n, digits_spanned)
+
+    sv = convert(Vector{Char},sinteger)
+    p = repeat(" ", n+(fullgroups-1)+(finalgroup!=0))
+    pretty = convert(Vector{Char},p)
+
+    sourceidx = n
+    targetidx = length(pretty)
+    for k in fullgroups:-1:1
+        pretty[(targetidx-digits_spanned+1):targetidx] = sv[(sourceidx-digits_spanned+1):sourceidx]
+        sourceidx -= digits_spanned
+        targetidx -= digits_spanned
+        if k > 1
+            pretty[targetidx] = group_separator
+            targetidx -= 1
+        end
+    end
+
+    if finalgroup > 0
+        if fullgroups > 0
+            pretty[targetidx] = group_separator
+            targetidx -= 1
+        end
+        pretty[(targetidx-finalgroup+1):targetidx] = sv[(sourceidx-finalgroup+1):sourceidx]
+    end
+
+    prettystring = convert(String, pretty)
+
+    if length(sexponent) != 0
+       string(prettystring,"e",sexponent)
+    else
+       prettystring
+    end
+end
+
+function readable_integer{I<:Integer}(s::String, digits_spanned::I, group_separator::Char)
+    if s[1] != "-"
+       readable_nonneg_integer(s, digits_spanned, group_separator)
+    else
+       s1 = string(s[2:end])
+       pretty = readable_nonneg_integer(s1, digits_spanned, group_separator)
+       string("-", pretty)
+    end
+end
+
+function readable_fraction{I<:Integer}(s::String, digits_spanned::I, group_separator::Char)
+    sfrac, sexponent =
+        if contains(s,"e")
+           split(s,'e')
+        else
+           s, ""
+        end
+
+    pretty = reverse(readable_nonneg_integer(reverse(sfrac), digits_spanned, group_separator))
+
+    if length(sexponent) != 0
+       string(pretty,"e",sexponent)
+    else
+       pretty
+    end
+end
