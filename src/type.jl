7ad7a75662cf79fd1a1987f8e337de6aa04ef5ba
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2016-12-17 05:43:27 -0500

    Update type.jl

--- src/type.jl
+++ src/type.jl
@@ -57,3 +57,17 @@ function set_pretty_number_style!(pns::PrettyNumberStyle)
     PRETTY_NUMBER_STYLE_HOLDER[1] = pns
     return nothing
 end    
+
+
+
+# accept extended precision numbers and make them become readable
+
+function readable{T<:Real}(x::T, pns::PrettyNumberStyle=get_pretty_number_style())
+    numstr = string(string(x),FRACPOINT)
+    return a_readable_number(numstr, pns)
+end
+
+function readable(x::String, pns::PrettyNumberStyle=get_pretty_number_style())
+    numstr = string(x, FRACPOINT)
+    return a_readable_number(numstr, pns)
+end    
