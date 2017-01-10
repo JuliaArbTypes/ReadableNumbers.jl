75e8081b30a6394e935f93b4aba5a0842c9b745b
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-01-09 21:37:34 -0500

    use Integer in parse_readable

--- src/iohelp.jl
+++ src/iohelp.jl
@@ -14,7 +14,7 @@ end
 
 # parse readable numeric strings
 
-parse_readable{T<:Union{Signed,AbstractFloat}}(::Type{T}, s::String, ch::Char) =
+parse_readable{T<:Union{Integer,AbstractFloat}}(::Type{T}, s::String, ch::Char) =
     Base.parse(T, join(split(s,ch),""))
 
 parse_readable{T<:AbstractFloat}(::Type{T}, s::String, ch1::Char, ch2::Char) =
