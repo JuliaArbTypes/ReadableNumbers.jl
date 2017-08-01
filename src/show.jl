3eee47a626fa4c3d2d6e6bcdd66b57557e89d9b0
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-08-01 04:05:28 -0400

    fixup for v0.7.0

--- src/show.jl
+++ src/show.jl
@@ -1,11 +1,12 @@
 # show 
 
-function show_readable{T<:Real}(io::IO, x::T)
+function show_readable(io::IO, x::T) where {T <: Real}
     str = readable(x)
     print(io, str)
 end
 
-function show_readable{T<:Real}(x::T)
+function show_readable(x::T) where {T <: Real}
     str = readable(x)
     print(STDOUT, str)
 end    
+
